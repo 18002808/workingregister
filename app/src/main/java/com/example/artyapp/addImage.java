d98936896ab75f22ef9a6a1e9a64cee9275ac64b
chi <18002808@brookes.ac.uk>
2022-03-03 10:14:03 -0800

    registration app

---
+++ app/src/main/java/com/example/artyapp/addImage.java
@@ -0,0 +1,14 @@
+package com.example.artyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addImage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addimage);
+    }
+}
--- app/src/main/java/com/example/artyapp/artistDashboard.java
+++ app/src/main/java/com/example/artyapp/artistDashboard.java
@@ -1,6 +1,7 @@
 package com.example.artyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 
 public class artistDashboard extends AppCompatActivity {
     TextView textViewName;
+    CardView insertImages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,7 @@ public class artistDashboard extends AppCompatActivity {
         }
 
         textViewName = findViewById(R.id.textViewName);
+        insertImages = findViewById(R.id.images);
         User user = SharedPrefManager.getInstance(this).getUser();
         textViewName.setText(user.getName());
 
@@ -29,6 +32,13 @@ public class artistDashboard extends AppCompatActivity {
             public void onClick(View view) {
                 finish();
                 SharedPrefManager.getInstance(getApplicationContext()).logout();
+            }
+        });
+
+        insertImages.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(artistDashboard.this, addImage.class));
             }
         });
     }
